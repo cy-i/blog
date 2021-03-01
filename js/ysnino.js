47156813b69daeff4edf4fe1d245a9293228c254
jc9820 <jadechan.vip@gmail.com>
2021-03-01 16:09:45 +0800

    update

---
+++ js/ysnino.js
@@ -0,0 +1,27 @@
+function fuckyou(){
+     window.close(); //关闭当前窗口(防抽)
+     window.location="https://www.ysni.cn/usr/themes/Typecho-Joe-Theme/console.html"; //将当前窗口跳转置空白页about:blank
+}
+function ck() {
+     console.profile();
+     console.profileEnd();
+     //我们判断一下profiles里面有没有东西，如果有，肯定有人按F12了，没错！！
+     if(console.clear) { console.clear() };
+     		if (typeof console.profiles =="object"){
+    				return console.profiles.length > 0;
+            }
+	 }
+function hehe(){
+	 if( (window.console && (console.firebug || console.table && /firebug/i.test(console.table()) )) || (typeof opera == 'object' && typeof opera.postError == 'function' && console.profile.length > 0)){
+	        fuckyou();
+	 }
+	 if(typeof console.profiles =="object"&&console.profiles.length > 0){
+			fuckyou();
+	 }
+}
+hehe();
+window.onresize = function(){
+	 if((window.outerHeight-window.innerHeight)>200)
+	 		//判断当前窗口内页高度和窗口高度，如果差值大于200，那么呵呵
+     		fuckyou();
+}
---
+++ js/ysnino2.js
@@ -0,0 +1,33 @@
+    $(document).ready(function () {
+      document.oncontextmenu = function () {
+        return false;
+      }
+      //document.onselectstart = function () {
+       // return false;
+     // }
+      //document.oncopy = function () {
+        //return false;
+     // }
+      document.onkeydown = function () {
+        //f12
+        if (window.event && window.event.keyCode == 123) {
+          event.keyCode = 0;
+          event.returnValue = false;
+          layer.msg("别看代码了=.=")
+          return false;
+        }
+        //ctrl+u
+        if (event.ctrlKey && window.event.keyCode == 85) {
+          return false;
+        }
+        //ctrl+shift+i
+        if ((event.ctrlKey) && (event.shiftKey) && (event.keyCode == 73)) {
+          return false;
+        }
+        // Ctrl+S
+        else if ((event.ctrlKey) && (event.keyCode == 83)) {
+          return false;
+        }
+      };
+
+    });
